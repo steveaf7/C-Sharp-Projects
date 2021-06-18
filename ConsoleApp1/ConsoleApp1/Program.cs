582fad4e7ea6e2458e1a8ff213045ea98fb82494
StevenFralix <steveaf7@gmail.com>
2021-06-18 15:06:25 -0700

    create a console app that functions as a daily report.

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //bool isStudying = false;
+            //byte hoursWorked = 42;
+            //sbyte currentTemperature = -23;
+            //char randomLetter = "X";
+            //char questionMark = '\u2103';
+            //decimal moneyInBank = 100.5m;
+
+            //double heightInCentimeters = 211.30202092;
+            //float secondsLeft = 2.62f;
+            //short temperatureOnMars = -341;
+            //string myName = "Steven";
+
+            //int currentAge = 30;
+            //string yearsOld = currentAge.ToString();
+            bool isRaining = true;
+            string rainingStatus = Convert.ToString(isRaining);
+
+            Console.WriteLine(isRaining);
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ DailyReport/DailyReport/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+namespace DailyReport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // prints the messages on separate lines. 
+            Console.WriteLine("The Tech Academy\nStudent Daily Report");
+            Console.WriteLine("What is your name?");
+            // saves the user input as a string at the location studentName
+            string studentName = Console.ReadLine();
+            Console.WriteLine("What course are you on?");
+            string studentCourse = Console.ReadLine();
+            Console.WriteLine("What page number?");
+            string pageNumberRaw = Console.ReadLine();
+            // converts string input to integer
+            int pageNumber = Convert.ToInt32(pageNumberRaw);
+            Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\".");
+            string needHelpRaw = Console.ReadLine();
+            // converts input to boolean for storage.
+            bool needHelp = Convert.ToBoolean(needHelpRaw);
+            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
+            string posExperiences = Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+            string feedback = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            string hoursStudiedRaw = Console.ReadLine();
+            int hoursStudied = Convert.ToInt32(hoursStudiedRaw);
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly.\nHave a Great day!");
+            // keeps the window open so the last line ca be read. 
+            Console.ReadLine();
+
+            
+
+
+
+        }
+    }
+}
