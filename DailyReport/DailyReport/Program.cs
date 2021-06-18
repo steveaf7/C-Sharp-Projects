43ce88b94ae0e145c281d3f4f88b4000029d8169
StevenFralix <steveaf7@gmail.com>
2021-06-18 15:37:32 -0700

    changed to convert variables on the same line as read them.

--- DailyReport/DailyReport/Program.cs
+++ DailyReport/DailyReport/Program.cs
@@ -15,20 +15,17 @@ namespace DailyReport
             Console.WriteLine("What course are you on?");
             string studentCourse = Console.ReadLine();
             Console.WriteLine("What page number?");
-            string pageNumberRaw = Console.ReadLine();
             // converts string input to integer
-            int pageNumber = Convert.ToInt32(pageNumberRaw);
+            int pageNumber = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Do you need help with anything? Please answer \"true\" or \"false\".");
-            string needHelpRaw = Console.ReadLine();
             // converts input to boolean for storage.
-            bool needHelp = Convert.ToBoolean(needHelpRaw);
+            bool needHelp = Convert.ToBoolean(Console.ReadLine());
             Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
             string posExperiences = Console.ReadLine();
             Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
             string feedback = Console.ReadLine();
             Console.WriteLine("How many hours did you study today?");
-            string hoursStudiedRaw = Console.ReadLine();
-            int hoursStudied = Convert.ToInt32(hoursStudiedRaw);
+            int hoursStudied = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly.\nHave a Great day!");
             // keeps the window open so the last line ca be read. 
             Console.ReadLine();
---
+++ MathOperations/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number: ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number: ");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            int sum = number1 + number2;
+            int difference = number1 - number2;
+            int product = number1 * number2;
+            int quotient = number1 / number2;
+            Console.WriteLine("Sum: " + sum + "\nDifference: " + difference + "\nProduct: " + product + "\nQuotient: " + quotient);
+            Console.ReadLine();
+        }
+    }
+}
