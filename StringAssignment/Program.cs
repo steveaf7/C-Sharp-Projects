7d22b824f266a9c7c854d97173917cdcb5131016
StevenFralix <steveaf7@gmail.com>
2021-06-22 12:37:11 -0700

    string assignment

---
+++ StringAssignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+namespace StringAssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            // string concatenation
+            string str1 = "This is the first part, ";
+            string str2 = "this is the second part, ";
+            string str3 = "and this is the third part of this sentence.";
+            string concatenated = str1 + str2 + str3;
+            Console.WriteLine(concatenated);
+
+            string lower = "uppercase";
+            string upper = lower.ToUpper();
+            // ToUpper() does not change the original string.
+            Console.WriteLine(lower);
+            Console.WriteLine(upper);
+
+            StringBuilder paragraph = new StringBuilder("This is the second sentence of the paragraph. ");
+            // Insert takes two arguments, the index to insert at, and the string to insert
+            paragraph.Insert(0, "This is the first sentence of the paragraph. ");
+            // Append adds to the end of the string
+            paragraph.Append("This is the third sentence of the paragraph. ");
+            paragraph.Append("This is the fourth sentence of the paragraph. ");
+            paragraph.Append("This is the fifth sentence of the paragraph. ");
+            Console.WriteLine(paragraph);
+            Console.Read();
+        }
+    }
+}
---
+++ StringFunctions/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "Steve";
+            //string quote = "The man said, \"Hello\", Jesse. \nHello on a new line. \n \tHello on a tab;";
+            //string fileName = "C:\\Users\\Steve";
+            //// instead of double slashes, you can also use the @ which tells the compiler to ignore the excape sequences. 
+            //string fileName1 = @"C:\Users\Jesse";
+
+            //bool trueOrFalse = name.Contains("s");
+            //Console.WriteLine(trueOrFalse);
+            //trueOrFalse = name.EndsWith("s");
+
+            //int length = name.Length;
+
+            //string nameUpper = name.ToUpper();
+            //string nameLower = name.ToLower();
+
+
+            //Console.WriteLine(length);
+            //Console.WriteLine(name);
+            //Console.WriteLine(nameUpper);
+            //Console.WriteLine(nameLower);
+            //Console.ReadLine();
+
+            //string name = "Jesse";
+            //name = "Joe";
+            //Console.WriteLine(name);
+            //Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("My name is Jesse");
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+
+        }
+    }
+}
