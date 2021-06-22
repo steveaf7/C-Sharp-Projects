d1525a38645c68fe12b67e0e0be13b4f916ea536
StevenFralix <steveaf7@gmail.com>
2021-06-21 17:31:44 -0700

    create shipping quote generator program.

---
+++ BranchingSubmissionAssignment/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+
+namespace BranchingSubmissionAssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            // print welcome message. get package weight from user, convert to integer.
+            Console.WriteLine("Welcome to package Express. Please follow the instructions below.\nPackage Weight? ");
+            // I used double for more exact math and because the user could enter a number with a decimal
+            double packageWeight = Convert.ToDouble(Console.ReadLine());
+
+            // if package weight greater than 50, print message, end program. 
+            if (packageWeight > 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express.\nHave a good day.");
+                Console.Read();
+            }
+
+            // if package weight not greater than 50, continue program
+            else
+            {
+                // get package width, height and length from user
+                Console.WriteLine("Package Width: ");
+                double packageWidth = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Package Height: ");
+                double packageHeight = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Package Length: ");
+                double packageLength = Convert.ToDouble(Console.ReadLine());
+
+                // if sum of length, width, and height greater than 50, print message and exit program.
+                if (packageWidth + packageHeight + packageLength > 50)
+                {
+                    Console.WriteLine("Package too heavy to be shipped via Package Express.\nHave a good day.");
+                    Console.Read();
+                }
+
+                // package meets requirements, continue to quote. 
+                else
+                {
+                    double quote = ((packageHeight * packageWidth * packageLength) * packageWeight) / 100;
+                    // prints message with calculated quote and formats to two decimal places. 
+                    Console.WriteLine("Your estimated total for shipping this package is: $" + String.Format("{0:0.00}", quote));
+                    Console.Read();
+                }
+            }
+
+            
+        }
+    }
+}
---
+++ ConditionalStatementsChallenge/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConditionalStatementsChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "jamie";
+            //if (name != "Jamie")
+            //{
+            //    Console.WriteLine("Hey Jamie");
+            //}
+            //else if (name == "Steve")
+            //{
+            //    Console.WriteLine("Hey Steve");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("You're not Jamie or Steve!");
+            //}
+            //Console.Read();
+            //int age = 21;
+            //if (age >= 18)
+            //{
+            //    Console.WriteLine("You are officially old enough to go to war. ");
+            //}
+            //if (age >= 21)
+            //{
+            //    Console.WriteLine("You are officially old enough to have a beer. Are you ready for your first beer?");
+            //}
+            //Console.Read();
+
+            //int num1 = 7;
+
+            //if (num1 < 18 && ((num1 == 7) || num1 == 2) && ((num1 == 8 && num1 > 20) || (num1 < 20 && num1 > 2)))
+            //{
+            //    Console.WriteLine("Wow, that was confusing.");
+            //}
+            int num1 = 12;
+            int num2 = 13;
+            string result = num1 < num2 ? "num1 is greater than num2" : "num1 is not greater than num2";
+            Console.WriteLine(result);
+            Console.Read();
+
+        }
+    }
+}
