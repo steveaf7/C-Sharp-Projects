3e228a4833638d851e7002ef276e7d99c1cd3480
StevenFralix <steveaf7@gmail.com>
2021-06-22 16:40:25 -0700

    console app array submission assignment

---
+++ ArraysAndLists/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+class Program
+{
+    static void Main()
+    {
+        List<int> intList = new List<int>();
+        intList.Add(4);
+        intList.Add(10);
+        intList.Remove(10);
+
+        List<string> strList = new List<string>();
+        strList.Add("Hello");
+        strList.Add("Steve");
+        strList.Remove("Steve");
+
+        Console.WriteLine(intList[0]);
+        Console.WriteLine(strList[0]);
+        Console.ReadLine();
+
+        
+
+        //int[] numArray = new int[5];
+        //numArray[0] = 5;
+        //numArray[1] = 2;
+        //numArray[2] = 10;
+        //numArray[3] = 200;
+        //numArray[4] = 5000;
+
+        ////same thing as above
+        //int[] numArray1 = new int[] { 5, 2, 10, 200, 5000 };
+
+        //int[] numArray2 = {5, 2, 10, 200, 5000, 600, 2300};
+        //numArray2[5] = 650;
+
+        //Console.WriteLine(numArray[4]);
+        //Console.WriteLine(numArray1[3]);
+        //Console.WriteLine(numArray2[5]);
+        //Console.ReadLine();
+    }
+}
+
---
+++ ConsoleAppArraySubmission/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Program
+{
+    static void Main()
+    {
+        // create a one dimensional Array of strings. 
+        string[] stringArray = { "Hello", "Welcome", "Hi", "What's up" };
+        // ask the user to select an index of the Array and then display the string at that index on the screen.
+        Console.WriteLine("Enter an index from 0 to 3");
+        int index = Convert.ToInt32(Console.ReadLine());
+        // add a message that displays when the user selects an index that doesn't exist
+        string result = (index > 3) ? "That index does not exist. " : stringArray[index];
+        Console.WriteLine(result);
+
+        // create a one dimensional Array of integers.
+        int[] intArray = { 7, 4, 432, 1654, 38 };
+        //ask the user to select an index of the Array and then display the string at that index on the screen. 
+        Console.WriteLine("Enter an index from 0 to 4");
+        index = Convert.ToInt32(Console.ReadLine());
+        result = (index > 4) ? "That index does not exist. " : Convert.ToString(intArray[index]);
+        Console.WriteLine(result);
+
+        // create a list of strings
+        List<string> strList = new List<string>();
+        strList.Add("Hello");
+        strList.Add("Hi");
+        strList.Add("What's up");
+        strList.Add("Welcome");
+        Console.WriteLine("Enter an index from 0 to 3");
+        index = Convert.ToInt32(Console.ReadLine());
+        result = (index > 3) ? "That index does not exist. " : strList[index];
+        Console.WriteLine(result);
+        Console.Read();
+
+
+    }
+}
+
