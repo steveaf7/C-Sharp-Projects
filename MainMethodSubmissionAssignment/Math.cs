70b58b9e9428815f9ad85e96e8f7b72f0f0b96ee
StevenFralix <steveaf7@gmail.com>
2021-06-24 16:00:34 -0700

    Main method submissio assignment

---
+++ MainMethodSubmissionAssignment/Math.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace MainMethodSubmissionAssignment
+{
+    // create a class
+    class Math
+    {
+        // create a method that will take an integer, perform a math operation, and the return as an integer. 
+        public static int Operation(int number)
+        {
+            number *= 20;
+            return number;
+        }
+
+        // add a second method to the class with teh same name that will take in a decimal, create a different math
+        // operation for it, then return the answer as an integer.
+        public static int Operation(decimal number)
+        {
+            int answer = (int)number / 15;
+            return answer;
+        }
+
+        // add a third method to this class, with the same name, that will take a string, convert it to an integer
+        // if possible, do a different math operation to it, then return the answer as an integer. 
+        public static int Operation(string input)
+        {
+            int number = Convert.ToInt32(input);
+            int answer = number * 500;
+            return answer;
+        }
+
+    }
+}
---
+++ MainMethodSubmissionAssignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace MainMethodSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiate the class and call the one method, passing in an integer, display to screen.
+            Math math = new Math();
+            Console.WriteLine(Math.Operation(20));
+            Console.ReadLine();
+
+            // instantiate the class and call the second method, passing in a decimal, display to screen.
+            Math math1 = new Math();
+            decimal dec = 213.9324m;
+            Console.WriteLine(Math.Operation(dec));
+            Console.ReadLine();
+
+            // instantiate the class and call the third method, passing in a string that equates to an integer
+            // display the results to the screen. 
+            Math math2 = new Math();
+            string input = "143";
+            Console.WriteLine(Math.Operation(input));
+            Console.ReadLine();
+
+        }
+    }
+}
