c3e394b7cf353c38d0ba22ecddc08e4136d5972b
StevenFralix <steveaf7@gmail.com>
2021-06-24 16:38:16 -0700

    remove static attribute from methods, updated program to call methods with instantiated objects.

---
+++ MethodSubmissionAssignment/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmissionAssignment
+{
+    class Class1
+    {
+        public static int method1(int dividend, int divisor = 5)
+        {
+            int answer = dividend / divisor;
+            return answer;
+        }
+    }
+}
---
+++ MethodSubmissionAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+            Console.WriteLine("Enter a number as a dividend: ");
+            int dividend = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a number as a divisor (leave blank for default = 5): ");
+            string input = Console.ReadLine();
+            if (input == "")
+            {
+                Console.WriteLine(Class1.method1(dividend));
+            }
+            else
+            {
+                int divisor = Convert.ToInt32(input);
+                Console.WriteLine(Class1.method1(dividend, divisor));
+            }
+            
+            Console.Read();
+        }
+    }
+}
