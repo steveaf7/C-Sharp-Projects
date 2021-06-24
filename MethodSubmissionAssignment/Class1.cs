ee8e99cf030b86e6c759b586e0703c864cc0d98c
StevenFralix <steveaf7@gmail.com>
2021-06-24 16:54:45 -0700

    method submission assignment

--- MethodSubmissionAssignment/Class1.cs
+++ MethodSubmissionAssignment/Class1.cs
@@ -1,14 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace MethodSubmissionAssignment
 {
+    // create a class
     class Class1
     {
-        public static int method1(int dividend, int divisor = 5)
+        // create method that takes two integers as parameters, one optional. 
+        // have the method do a math operation and return an integer result.
+        public int method1(int dividend, int divisor = 5)
         {
             int answer = dividend / divisor;
             return answer;
--- MethodSubmissionAssignment/Program.cs
+++ MethodSubmissionAssignment/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace MethodSubmissionAssignment
 {
@@ -10,21 +7,29 @@ namespace MethodSubmissionAssignment
     {
         static void Main(string[] args)
         {
+            // instantiate the class
             Class1 class1 = new Class1();
+
+            // ask the user to input two numbers, one at a time.
             Console.WriteLine("Enter a number as a dividend: ");
             int dividend = Convert.ToInt32(Console.ReadLine());
+            // let them know they need not enter anything for the second number
             Console.WriteLine("Enter a number as a divisor (leave blank for default = 5): ");
             string input = Console.ReadLine();
+
+            // call the method in the class, passing in the one or two numbers entered. 
+            // if input is blank, calls the method with one argument
+            // else, converts input to integer, and calls the method specifying two arguments
             if (input == "")
             {
-                Console.WriteLine(Class1.method1(dividend));
+                Console.WriteLine(dividend + " /  5 = " + class1.method1(dividend));
             }
             else
             {
                 int divisor = Convert.ToInt32(input);
-                Console.WriteLine(Class1.method1(dividend, divisor));
+                Console.WriteLine("{0} / {1} = {2}", dividend, divisor, class1.method1(dividend, divisor));
             }
-            
+
             Console.Read();
         }
     }
