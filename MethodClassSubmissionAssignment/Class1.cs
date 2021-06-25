7f0bffae4f411be753d29ee6ef51c4e1ac66abff
StevenFralix <steveaf7@gmail.com>
2021-06-24 17:05:53 -0700

    Method Class assignment

---
+++ MethodClassSubmissionAssignment/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace MethodClassSubmissionAssignment
+{
+    // create a class.
+    class Class1
+    {
+        // create a void method that takes two integers as parameters. 
+        public void method(int number1, int number2)
+        {
+            // have the method do a math operation on the first integer and display the second integer to the screen.
+            number1 += number1;
+            Console.WriteLine(number2);
+        }
+    }
+}
---
+++ MethodClassSubmissionAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace MethodClassSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiate the class.
+            Class1 class1 = new Class1();
+            // call the method in the class, passing in two numbers. 
+            class1.method(21, 147);
+            // call the method in the class, specifying the parameters by name. 
+            class1.method(number1: 27, number2: 12);
+            Console.ReadLine();
+
+        }
+    }
+}
