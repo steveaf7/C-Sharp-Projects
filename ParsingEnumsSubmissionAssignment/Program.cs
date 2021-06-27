4121e787aa7f39f31bb0ac9242da9d23db88df7e
StevenFralix <steveaf7@gmail.com>
2021-06-26 19:33:25 -0700

    added a message if the conversion is successful. convert input to lowercase to normalize.

--- ParsingEnumsSubmissionAssignment/Program.cs
+++ ParsingEnumsSubmissionAssignment/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Text;
 
 namespace ParsingEnumsSubmissionAssignment
 {
@@ -10,31 +10,32 @@ namespace ParsingEnumsSubmissionAssignment
             // prompt the user to enter the current day of the week.
             Console.WriteLine("Enter the current day of the week: ");
             // assign the value to a variable of that enum data type you just created. 
-            string input = Console.ReadLine();
+            string input = Console.ReadLine().ToLower();
             try
             {
                 // converts input into an enum (if it corresponds with an element in enum)
                 Enum.Parse(typeof(DaysOfTheWeek), input);
+                Console.WriteLine("Conversion Successful");
             }
 
             catch (Exception)
             {
                 Console.WriteLine("Please enter an actual day of the week. ");
             }
-
+           
             Console.ReadLine();
 
         }
         // create an enum for days of the week. 
         public enum DaysOfTheWeek
         {
-            Monday,
-            Tuesday,
-            Wednesday,
-            Thursday,
-            Friday,
-            Saturday,
-            Sunday
+            monday,
+            tuesday,
+            wednesday,
+            thursday,
+            friday,
+            saturday,
+            sunday
         }
     }
 }
