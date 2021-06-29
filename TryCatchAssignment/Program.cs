f3164d500dc602e556825832c9a2b7e7b188e085
StevenFralix <steveaf7@gmail.com>
2021-06-29 16:40:06 -0700

    Try Catch assignment

---
+++ TryCatchAssignment/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ask the user for their age
+            bool validAnswer = false;
+            int age = 0;
+
+            while (!validAnswer || age <= 0)
+            {
+                try
+                {
+                    Console.WriteLine("enter your age: ");
+                    validAnswer = int.TryParse(Console.ReadLine(), out age);
+                    if (!validAnswer) Console.WriteLine("Please enter digits only, no decimals.");
+                    if (age <= 0)
+                    {
+                        throw new NegativeOrZeroException();
+                    }
+                }
+                catch (NegativeOrZeroException)
+                {
+                    Console.WriteLine("Please enter a positive integer. ");
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Error occured. Contact your system administrator.");
+                    Console.ReadLine();
+                }
+
+            }
+               
+            // display the year the user was born
+            Console.WriteLine("You were born in {0}", 2021 - age);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
--- TwentyOne/Program.cs
+++ TwentyOne/Program.cs
@@ -12,13 +12,20 @@ namespace TwentyOne
         {
             const string casinoName = "Grand Hotel and Casino";
 
-            
-            
+
             Console.WriteLine("Welcome to the {0}. Let's start by telling me your name.", casinoName);
             string playerName = Console.ReadLine();
+            
+            bool validAnswer = false;
+            int bank = 0;
+            while (!validAnswer)
+            {
+                Console.WriteLine("And how much money did you bring today");
+                validAnswer = int.TryParse(Console.ReadLine(), out bank);
+                if (!validAnswer) Console.WriteLine("Please enter digits only, no decimals");
+            }
 
-            Console.WriteLine("And how much money did you bring today?");
-            int bank = Convert.ToInt32(Console.ReadLine());
+
 
             Console.WriteLine("Hello, {0}, Would you like to join a game of 21 right now?", playerName);
             string answer = Console.ReadLine().ToLower();
@@ -36,7 +43,24 @@ namespace TwentyOne
                 player.IsActivelyPlaying = true;
                 while (player.IsActivelyPlaying && player.Balance > 0)
                 {
-                    game.Play();
+                    try
+                    {
+                        game.Play();
+                    }
+                    // start with the more specific exceptions and have general exceptions down below. 
+                    catch (FraudException)
+                    {
+                        Console.WriteLine("Security. Kick this person out");
+                        Console.ReadLine();
+                        return;
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("An error occured. Please contact your system administrator.");
+                        Console.ReadLine();
+                        return;
+                    }
+                    
                 }
                 game -= player;
                 Console.WriteLine("Thank you for playing!");
